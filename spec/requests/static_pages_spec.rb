8f8545c9907437f79883e71c7fb95dc6cddd9b38
Simon Rotter <simon.rotter@fu-berlin.de>
2013-07-19 12:52:25 +0200

    finishing section 6.2

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe "Static pages" do
 
   let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+  #shared_examples_for "all static pages" do
+  #  it { should have_content(heading) }
+  #  it { should have_title(full_title(page_title)) }
+  #end
 
   subject { page }
 
@@ -33,6 +37,21 @@ describe "Static pages" do
 
     it { should have_content('Contact') }
     it { should have_title(full_title('Contact')) }
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign up'))
+    click_link "sample app"
+    expect(page).to have_title(full_title(''))
   end
 end
 
